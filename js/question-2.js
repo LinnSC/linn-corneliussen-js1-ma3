a9eb99d3c886695cf3179d784def569f7b13167e
LinnSC <71352428+LinnSC@users.noreply.github.com>
2021-02-07 23:03:11 +0100

    Added Q2

--- js/question-2.js
+++ js/question-2.js
@@ -17,3 +17,40 @@
 
 // Be sure to arrange all file types appropriately, consult the
 // repos from the lessons for examples.
+
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+  try {
+    const response = await fetch(url);
+    const gamesResults = await response.json();
+
+    const gamesInfo = gamesResults.results;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < gamesInfo.length; i++) {
+      console.log(gamesInfo[i].name);
+
+      if (i === 8) {
+        break;
+      }
+
+      resultsContainer.innerHTML += `<div class="result">
+    <p>Name: ${gamesInfo[i].name} </p>
+    <p>Rating: ${gamesInfo[i].rating}</p>
+    <p>Tags: ${gamesInfo[i].tags.length}</p>
+  </div>`;
+    }
+  } catch (error) {
+    console.log("An error occured");
+    resultsContainer.innerHTML = errorMessage(
+      "An error occured when getting the game info"
+    );
+  }
+}
+
+getGames();

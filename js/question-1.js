7dc388768bd46a1d2f959287a5164f431546f050
LinnSC <71352428+LinnSC@users.noreply.github.com>
2021-02-07 16:54:17 +0100

    .

--- js/question-1.js
+++ js/question-1.js
@@ -1,5 +1,10 @@
 // Question 1
 // Convert the function below to an arrow function:
-// function division(a, b) {
-//     return a % b;
-// }
+
+
+
+function division(a, b) {
+   
+   
+             return a % b;
+}
